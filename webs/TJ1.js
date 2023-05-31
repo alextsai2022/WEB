fa6cb5144fab595128752f80a1aad2c591006557
alextsai2022 <113331936+alextsai2022@users.noreply.github.com>
2023-05-31 19:10:55 +0800

    Add files via upload

---
+++ webs/TJ1.js
@@ -0,0 +1,9 @@
+function f1() {
+    document.getElementById("y1").innerHTML = "台灣國家公園";
+    document.getElementById("y2").innerHTML = "國家網頁測試";
+}
+function f2(){
+    document.getElementById("y1").innerHTML = "玉山國家公園";
+    document.getElementById("y2").innerHTML = "這是網頁測試";
+
+}
---
+++ webs/TJ2.js
@@ -0,0 +1,28 @@
+function f1(){
+    document.getElementById("A1").innerHTML='Cat'
+}
+function f2(){
+    document.getElementById("A2").innerHTML='456'
+}
+function f3(){
+    document.getElementById("A3").innerHTML='XYZ'
+}
+function f4(){
+    document.write("NO")
+}
+function f6(){
+    var n1="Hello";
+    var n2="World";
+    document.getElementById("A4").innerHTML=n1
+    document.getElementById("A5").innerHTML=n2
+    document.getElementById("A6").innerHTML=n1+n2
+}
+function f7(){
+    var x=10,y=0101,z=0x11;
+    x
+    y
+    z
+}
+function f8(){
+    document.getElementById("A8").innerHTML='Hi \"Hi"'
+}
